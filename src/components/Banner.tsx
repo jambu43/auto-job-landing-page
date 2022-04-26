8dfbce24b38a32e77a2daefbc07095d35523bc69
Joel AMBU <ambujoel@gmail.com>
2022-04-26 07:03:36 +0100

    Banner home

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import {
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 
-function Banner() {
-  return <div>Banner</div>;
+export default function Banner() {
+  return (
+    <Stack minH={"30vh"} direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} justify={"center"}>
+        <Stack spacing={6} w={"full"} maxW={"lg"}>
+          <Stack direction={{ base: "column", md: "row" }} spacing={4}>
+            <Button
+              rounded={"full"}
+              bg={"#FFFAF9"}
+              color={"#FB4718"}
+              _hover={{
+                bg: "#FB4718",
+                color: "#FFFAF9",
+              }}
+            >
+              Hello! This is Autojob
+            </Button>
+          </Stack>
+          <Heading fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}>
+            <Text color={"#000"} as={"span"}>
+              We look for your next job, we find it and it’s 100% free!
+            </Text>
+          </Heading>
+          <Text fontSize={{ base: "md", lg: "lg" }} color={"#000"}>
+            Upload your resume and autojob will automatically apply for new jobs
+            on your behalf that match your profile.
+          </Text>
+          <Stack direction={{ base: "column", md: "row" }} spacing={4}>
+            <Button
+              rounded={"full"}
+              bg={"#FB4718"}
+              color={"#FFFAF9"}
+              _hover={{
+                bg: "#FB4718",
+                color: "#FFFAF9",
+              }}
+            >
+              Get Started Now
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={"Login Image"}
+          objectFit={"cover"}
+          src={"/images/Illustrator.svg"}
+        />
+      </Flex>
+    </Stack>
+  );
 }
-
-export default Banner;
